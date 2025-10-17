62e8a2da23e664b75e4a3011d278db6cdcb32b96
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-10-17 09:39:10 +0100

    README file for exercise 2 updated

--- practice-excerise-2/src/components/Profile.jsx
+++ practice-excerise-2/src/components/Profile.jsx
@@ -1,8 +1,10 @@
+import profilePic from "../assets/profile1.jpeg";
+
 function Profile(){
 
     return (
-        <div>
-            <img src="./assets/profile1.jpeg" alt="Profile" />
+        <div className="profile">
+            <img src={profilePic} alt="Profile" />
             <h2>Adesoji Ajijolaoluwa David</h2>
             <h3>AI Developer</h3>
             <p>I am a dedicated programmer who uses his knowledge to tackle real world problems.</p>
