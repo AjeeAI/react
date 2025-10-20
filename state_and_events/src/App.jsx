53eb00985d45fb06784c2c827cf2f525beff2733
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-10-20 10:43:26 +0100

    Readme for day 2 task 1

---
+++ state_and_events/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./components/Counter.jsx"
+import UserProfile from "./components/UserProfile.jsx";
+
+
+function App(){
+  return (
+    <div>
+      <Counter/>
+    <Counter/>
+    <UserProfile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ state_and_events/src/components/UserProfile.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function UserProfile(){
+    const [user, setUser] = useState({
+        name: "Chidi",
+        age: 25,
+        city: "Lagos"
+    });
+
+    function updateCity(){
+        setUser({
+            ...user,
+            city: "Abuja"
+        });
+    }
+
+    return (
+        <div>
+            <div>
+                {user}
+            </div>
+            <button onClick={updateCity}>Move to Abuja</button>
+        </div>
+    );
+}
+
+export default UserProfile;
